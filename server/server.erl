84d31318b982a0b859271623212b9a8bb5a9093a
José Pedro <72660243+JosePedro-31@users.noreply.github.com>
2025-04-16 12:08:54 +0100

    correções no servidor

--- server/server.erl
+++ server/server.erl
@@ -9,10 +9,26 @@ server(Port) ->
     Result = gen_tcp:listen(Port, [binary, {packet, line}]),
     case Result of
         {ok, LSock} ->
-            ok;
+            io:fwrite("Server listening! Waiting for connections...\n"),
+            accept(LSock);
         {error, Reason} ->
             io:fwrite("Error generating socket\n"),
             Reason
+    end.
+
+
+accept(LSocket) ->
+    case gen_tcp:accept(LSocket) of
+        {ok, ClientSocket} ->
+            io:fwrite("Client connected: ~p~n", [ClientSocket]),
+            % IMPORTANTE: Aqui deverias iniciar um novo processo para lidar com este cliente!
+            % Exemplo muito simples: spawn(fun() -> handle_client(ClientSocket) end),
+            gen_tcp:send(ClientSocket, "Welcome to the server!\n"), % Envia mensagem ao cliente (com newline!)
+            % Chamada recursiva para aceitar a próxima conexão
+            accept(LSocket);
+        {error, Reason} ->
+            io:fwrite("Error accepting connection: ~p~n", [Reason])
+            % Talvez parar o servidor ou tentar novamente dependendo do erro
     end.
 
 
