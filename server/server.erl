c9bd5e6aca0bf89238d2a400b9ef36713c561098
José Pedro <72660243+JosePedro-31@users.noreply.github.com>
2025-04-16 11:20:02 +0100

    teste

--- server/server.erl
+++ server/server.erl
@@ -3,6 +3,8 @@
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
+% teste
+
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
